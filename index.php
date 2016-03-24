3f325feb92c1fcb08f6c1d3483efbab37c917750
unknown <laura.cccruz@CAS411483W1017.senacsp.edu.br>
2016-03-23 21:59:10 -0300

    aula

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿<?php
 include ('dados.php');
 if (isset($_POST['btn_send'])) {
-	
+	$connect = mysqli_connect (host_db, login, senha, Nome);
 	if (isset($logins[$_POST['login']])) {
 		
 		if ($logins[$_POST['login']] == $_POST['senha']) {
@@ -16,13 +16,13 @@ if (isset($_POST['btn_send'])) {
 	
 	} else {
 				$msg = 'Usuário ou Senha Inválidos';
-				include('../templates/login_tpl.php');		
+				include('/templates/login_tpl.php');		
 						
 	}
 	
 } else {
 	
-	include('../templates/login_tpl.php');	
+	include('/templates/login_tpl.php');	
 	
 }
 ?>
