45d6908223597c58abf71c8f8991643788ad1225
unknown <laura.cccruz@CAS411483W1017.senacsp.edu.br>
2016-03-23 22:24:29 -0300

    aula 23

--- dados.php
+++ dados.php
@@ -2,5 +2,9 @@
 define('Nome', 'db');
 define ('senha', '');
 define ('login', 'root');
-define ('host_db', 'localhost');
+define ('host_db', '127.0.0.1');
+
+$connect = mysqli_connect (host_db, login, senha, Nome);
+echo '<pre>';
+print_r($connect);
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿<?php
 include ('dados.php');
 if (isset($_POST['btn_send'])) {
-	$connect = mysqli_connect (host_db, login, senha, Nome);
+	
 	if (isset($logins[$_POST['login']])) {
 		
 		if ($logins[$_POST['login']] == $_POST['senha']) {
