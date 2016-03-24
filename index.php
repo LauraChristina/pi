a7142fa8a85bf83360d4624ea05ba727261c50c5
unknown <laura.cccruz@CAS411483W1017.senacsp.edu.br>
2016-03-23 21:28:08 -0300

    dia 23

---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+if (isset($_POST['btn_send'])) {
+	
+	if (isset($logins[$_POST['login']])) {
+		
+		if ($logins[$_POST['login']] == $_POST['senha']) {
+			
+			include('../templates/admin_tpl.php');
+			
+		} else {
+					$msg = 'Usuário ou Senha Inválidos';
+					include('../templates/login_tpl.php');		
+				
+	}
+	
+	} else {
+				$msg = 'Usuário ou Senha Inválidos';
+				include('../templates/login_tpl.php');		
+						
+	}
+	
+} else {
+	
+	include('../templates/login_tpl.php');	
+	
+}
+?>
---
+++ templates/admin_tpl.php
@@ -0,0 +1,11 @@
+﻿<html>
+	<head>
+	</head>
+	<body>
+		<br><br>
+		<center>
+			<b>Bem-vindo(a)</b><br><br>
+			Tela de admin
+		</center>
+	</body>
+</html>
